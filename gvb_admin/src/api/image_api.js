ffa21c1d89881d5b4678011a4b2355de725c4b33
huchenxi <2387360024@qq.com>
2024-10-29 18:32:23 +0800

    删除gvb_admin,整合主页后台

--- gvb_admin/src/api/image_api.js
+++
@@ -1,5 +0,0 @@
-import http from './request'
-
-export function imageUpdateName(data) {
-    return http.put('/images', data)
-}
--- gvb_admin/src/utils/jwt.js
+++
@@ -1,7 +0,0 @@
-// 解码jwt
-export function ParseToken(token) {
-    let payload = token.split(".")[1]
-
-    let userInfo = decodeURIComponent(escape(window.atob(payload.replace(/-/g, "+").replace(/_/g, "/"))))
-    return JSON.parse(userInfo)
-}
